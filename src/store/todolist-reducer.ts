cfad99abf9563ce724082b05b438eca973cdbcc6
Sergej Garkusha <Tenebrann@googlemail.com>
2023-03-18 18:00:01 +0100

    added todolist-reducer

---
+++ src/store/todolist-reducer.ts
@@ -0,0 +1,21 @@
+// 1. Должна заменить прежний зоопарк
+// 2. Задача - только преобразование стэйта
+// 3. Вернуть новый стэйт
+import { TodoListType } from '../App';
+
+const REMOVE_TODOLIST = 'REMOVE-TODOLIST' as const;
+
+type RemoveTodoListAT = {
+  type: typeof REMOVE_TODOLIST;
+  id: string;
+};
+
+export const todolistReducer = (todolists: TodoListType[], action: RemoveTodoListAT) => {
+  switch (action.type) {
+    case 'REMOVE-TODOLIST':
+      return todolists.filter(tl => tl.id !== action.id);
+
+    default:
+      return todolists;
+  }
+};
